66ebc9c40232388c2d1c7633b567bb1f7807244b
Naman Gupta <naman312000@gmail.com>
2021-09-24 14:46:12 +0530

    initial commit

---
+++ reactapp/components/src/component/weather/temp.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./style.css";
+
+const Temp = () => {
+    return (
+        <>
+        <div className="wrap">
+            <div className="search">
+
+            </div>
+
+        </div>
+            
+        </>
+    )
+}
+
+export default Temp
